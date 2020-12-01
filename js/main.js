99e08b85ebc03720ee05850181344c63da368084
DevDams <ad.aimedesire@gmail.com>
2020-12-01 18:33:25 +0000

    Added the javascript for the transition in the project page

--- js/main.js
+++ js/main.js
@@ -96,17 +96,95 @@ $($overlay).mouseout(function() {
 
 
 
-/* PROJECT BOX HORIZONTAL SCROLL IN PROJECT PAGE */
-var blocks = document.getElementsByClassName('block');
-var container = document.getElementsByClassName('project-box');
-var hs = new HorizontalScroll.default({
-	blocks : blocks,
-	container: container,
-  isAnimated: true,
-  springEffect: 0.8,
+/* TRANSITION IN PROJECT PAGE */
+var $activeSlide = $('.active'),
+$homeSlide = $(".slide"),
+$slideNavPrev = $("#prev"),
+$slideNavNext = $("#next");
+
+function init() {
+      TweenMax.set($homeSlide.not($activeSlide), {autoAlpha: 0});
+      TweenMax.set($slideNavPrev, {autoAlpha: 0.2});
+}
+
+init();
+
+function goToNextSlide(slideOut, slideIn, slideInAll) {
+      var t1 = new TimelineLite(),
+      slideOutContent = slideOut.find('.box-content'),
+      slideInContent = slideIn.find('.box-content'),
+      slideOutImg = slideOut.find('.box-img'),
+      slideInImg = slideIn.find('.box-img'),
+      index = slideIn.index(),
+      size = $homeSlide.length;
+      console.log(index);
+
+      if(slideIn.length !== 0) {
+      t1
+      .set(slideIn, {autoAlpha: 1, className: '+=active'})
+      .set(slideOut, {className: '-=active'})
+      .to(slideOutContent, 0.65, {y: "+=40px", ease: Power3.easeInOut}, 0)
+      .to(slideOutImg, 0.65, {backgroundPosition :'bottom', ease: Power3.easeInOut}, 0)
+      .to(slideInAll, 1, {y: "-=100%", ease: Power3.easeInOut}, 0)
+      .fromTo(slideInContent, 0.65, {y: '-=40px'}, {y : 0, ease: Power3.easeInOut}, "-=0.7")
+      .fromTo(slideInImg, 0.65, {backgroundPosition: 'top'}, {backgroundPosition: 'bottom', ease: Power3.easeInOut}, '-=0.7')
+      }
+
+      TweenMax.set($slideNavPrev, {autoAlpha: 1});
+
+      if(index === size - 1){
+            TweenMax.to($slideNavNext, 0.3, {autoAlpha: 0.2, ease:Linear.easeNone});
+      }
+};
+
+$slideNavNext.click(function(e) {
+      e.preventDefault();
+
+      var slideOut = $('.slide.active'),
+      slideIn = $('.slide.active').next('.slide'),
+      slideInAll = $('.slide');
+
+      goToNextSlide(slideOut, slideIn, slideInAll);
+
 });
 
+function goToPrevSlide(slideOut, slideIn, slideInAll) {
+      var t1 = new TimelineLite(),
+      slideOutContent = slideOut.find('.box-content'),
+      slideInContent = slideIn.find('.box-content'),
+      slideOutImg = slideOut.find('.box-img'),
+      slideInImg = slideIn.find('.box-img'),
+      index = slideIn.index(),
+      size = $homeSlide.length;
 
+      if(slideIn.length !== 0) {
+      t1
+      .set(slideIn, {autoAlpha: 1, className: '+=active'})
+      .set(slideOut, {className: '-=active'})
+      .to(slideOutContent, 0.65, {y: "-=40px", ease: Power3.easeInOut}, 0)
+      .to(slideOutImg, 0.65, {backgroundPosition :'top', ease: Power3.easeInOut}, 0)
+      .to(slideInAll, 1, {y: "+=100%", ease: Power3.easeInOut}, 0)
+      .fromTo(slideInContent, 0.65, {y: '+=40px'}, {y : 0, ease: Power3.easeInOut}, "-=0.7")
+      .fromTo(slideInImg, 0.65, {backgroundPosition: 'bottom'}, {backgroundPosition: 'top', ease: Power3.easeInOut}, '-=0.7')
+      }
+
+      TweenMax.set($slideNavPrev, {autoAlpha: 1});
+
+      if(index === 0){
+            TweenMax.to($slideNavPrev, 0.3, {autoAlpha: 0.2, ease:Linear.easeNone});
+      }
+};
+
+$slideNavPrev.click(function(e) {
+      e.preventDefault();
+
+      var slideOut = $('.slide.active'),
+      slideIn = $('.slide.active').prev('.slide'),
+      slideInAll = $('.slide');
+
+      goToPrevSlide(slideOut, slideIn, slideInAll);
+
+});
 
 /***
  * PANZOOM JS FOR DRAGGABLE CANVA
